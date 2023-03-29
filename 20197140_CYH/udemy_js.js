7a6c3107500b23740ce39bb4f44c63fae14be841
yschoo0 <yyssms@daum.net>
2023-03-29 21:38:54 +0900

    second-변수, 연산자

---
+++ 20197140_CYH/udemy_js.js
@@ -0,0 +1,85 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "YiHyeon";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+
+let Person = 'jonas'
+let PI = 3.1415;
+console.log(PI);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'teacher';
+console.log(myFirstJob);*/
+
+/*let javascriptsFun = true;
+console.log(javascriptsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptsFun);
+//console.log(typeof 23);
+//console.log(typeof 'YiHyeon');
+
+javascriptsFun = 'YES!';
+console.log(typeof javascriptsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+
+var job = 'programmer';
+job = 'teacer';
+//console.log(job);
+//console.log(typeof job);
+
+lastName = 'Cho_';
+console.log(lastName);
+*/
+// Math operators
+const now = 2047;
+const ageYihyeon =  now - 2000;
+const ageSarah  = now - 2018;
+console.log(ageYihyeon, ageSarah);
+
+console.log(ageYihyeon *2 , ageYihyeon / 2, 2 ** 3)
+// 2**3 means 2 to the power of 3 = 2*2*2
+
+
+const firstName = 'YiHyeon';
+const lastName = 'Chooo';
+console.log(firstName + ' '+ lastName);
+
+//Assignment operators
+let x = 10 + 5; // 15
+x += 10; //x = x+10 = 25
+x *= 4; // x = x*4 = 100
+x++; // x= x+1
+x--;
+x--;
+console.log(x);
+
+//Comparison operators(비교연산자)
